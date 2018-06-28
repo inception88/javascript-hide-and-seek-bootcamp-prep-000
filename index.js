762d05604b8f09ff61874490b5893702f3356f16
Benjamin Jones <bljones36@gmail.com>
2018-06-28 04:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function deepestChild() {
   (x = x+' div');
  z = document.querySelector('x');
  if (z===null){
-   return x;
+   p= document.querySelector('y');return p;
  }return x;
   }return x;
   }
