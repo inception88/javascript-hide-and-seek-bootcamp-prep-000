6b28f57ec00cf250d5af408f5a869fdf4bec19ea
Benjamin Jones <bljones36@gmail.com>
2018-06-28 05:08:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector){
+  let x = document.querySelector(selector);
+  return x;
+}
+
+function nestedTarget() {
+ let x = document.querySelector('#nested .target');
+ return x;
+}
+
+function increaseRankBy(n) {
+ let x = document.querySelectorAll('.ranked-list');
+for (let i = 0; i < x.length; i++) {
+  x[i].innerHTML = (i + n).toString();
+}
+}
+
+function deepestChild() {
+ var x = ('#grand-node');
+  while (z!==null) {
+  var y=document.querySelector(x);
+  (x = x+' div');
+var z = document.querySelector(x);
+ }
+  return y;
+  }
